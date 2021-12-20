f980661d99b4e2042d2637f95825b0655bb36dfd
emaema7131 <emaema7131@gmail.com>
2021-12-21 00:46:35 +0530

    minor correction

--- db/checkJwt.js
+++ db/checkJwt.js
@@ -1,33 +1,39 @@
 var jwt = require('jsonwebtoken');
 const { client } = require('./connection');
 
-const CheckUser = async (req, res, next) => {
+module.exports.CheckUser = async function (req, res, next) {
     const auth = req.headers['authorization'];
-    if (auth){
-        var decoded = jwt.verify(auth, process.env.AUTH_KEY);
-        if (decoded){
-            client.query("SELECT * FROM users WHERE id = $1", [decoded], (err, result) => {
-                if (err) {
-                    res.send({
-                        message: "Not Allowed to access this api"
-                    });
-                } else {
-                    req.params['user_id'] = result.rows[0].id;
-                    next();
-                }
-            })
-        }else{
+    if (auth) {
+        try {
+            var decoded = jwt.verify(auth, process.env.AUTH_KEY);
+            if (decoded) {
+                client.query("SELECT * FROM users WHERE id = $1", [decoded], (err, result) => {
+                    if (err) {
+                        res.send({
+                            message: "Not Allowed to access this api"
+                        });
+                    } else if (result && result.rows && result.rows[0]) {
+                        req.params['user_id'] = result.rows[0].id;
+                        next();
+                    } else {
+                        res.send({
+                            message: "No user found on this credential"
+                        });
+                    }
+                })
+            } else {
+                res.send({
+                    message: "Provide Valid Auth token"
+                });
+            }
+        } catch (error) {
             res.send({
-                message: "Provide Valid Auth token"
+                message: error.message
             });
         }
-    }else{
+    } else {
         res.send({
             message: "Need Authorizations"
         });
     }
 }
-
-module.exports = {
-    CheckUser
-};
